a4f77ddf7d30a51d118bca3ed33f37520a921ed5
Vojtech Vitek <vojtech.vitek@gmail.com>
2022-09-27 12:47:05 +0200

    Go client: implement sendMessage(); WIP of subscribe()

--- client-go/main.go
+++ client-go/main.go
@@ -4,133 +4,57 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
+	"os"
+	"os/user"
 	"strings"
-	"sync"
 	"time"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 )
 
 func main() {
-	err := startServer()
-	if err != nil {
+	if len(os.Args) > 2 {
+		if err := sendMessage(strings.Join(os.Args[1:], " ")); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	if err := subscribe(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func startServer() error {
-	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
+func sendMessage(msg string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 
-	cors := cors.New(cors.Options{
-		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"POST", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	})
-	r.Use(cors.Handler)
+	chatbot := NewChatClient("http://localhost:4242", http.DefaultClient)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("."))
-	})
-
-	webrpcHandler := NewChatbotServer(&RPC{
-		msgId: 3,
-		msgs: []*Message{
-			{
-				ID:     1,
-				Author: "Test",
-				Msg:    "Message 1",
-			},
-			{
-				ID:     2,
-				Author: "Test",
-				Msg:    "Message 2",
-			},
-			{
-				ID:     3,
-				Author: "Test",
-				Msg:    "Message 3",
-			},
-		},
-	})
-	r.Handle("/*", webrpcHandler)
-
-	return http.ListenAndServe(":4242", r)
-}
-
-type RPC struct {
-	msgLock sync.RWMutex
-	msgId   uint64
-	msgs    []*Message
-}
-
-func (s *RPC) generateMessage() *Message {
-	s.msgLock.Lock()
-	defer s.msgLock.Unlock()
-
-	s.msgId++
-	msg := &Message{
-		ID:     s.msgId,
-		Author: "Test",
-		Msg:    fmt.Sprintf("Message %v", s.msgId),
-	}
-	s.msgs = append(s.msgs, msg)
-	return msg
-}
-
-func (s *RPC) SendMessage(ctx context.Context, author string, msg string) (bool, error) {
-	author = strings.TrimSpace(author)
-	if msg == "" {
-		return false, ErrorInvalidArgument("author", "empty author")
+	author := "Go client"
+	if user, err := user.Current(); err == nil {
+		author = user.Username
 	}
 
-	msg = strings.TrimSpace(msg)
-	if msg == "" {
-		return false, ErrorInvalidArgument("msg", "empty message")
+	_, err := chatbot.SendMessage(ctx, author, msg)
+	if err != nil {
+		return err
 	}
 
-	s.msgLock.RLock()
-	defer s.msgLock.RUnlock()
-
-	s.msgId++
-	s.msgs = append(s.msgs, &Message{
-		ID:        s.msgId,
-		Author:    author,
-		CreatedAt: time.Now(),
-		Msg:       msg,
-	})
-	return true, nil
+	return nil
 }
 
-func (s *RPC) SubscribeMessages(ctx context.Context) (chan *Message, error) {
-	msgs := make(chan *Message, 100)
+func subscribe() error {
+	ctx := context.Background()
 
-	go func() {
-		func() {
-			s.msgLock.RLock()
-			defer s.msgLock.RUnlock()
+	chatbot := NewChatClient("http://localhost:4242", http.DefaultClient)
+	msgs, err := chatbot.SubscribeMessages(ctx)
+	if err != nil {
+		return err
+	}
 
-			for _, msg := range s.msgs {
-				msgs <- msg
-			}
-		}()
+	for _, msg := range msgs {
+		fmt.Printf("%s: %s", msg.Author, msg.Msg)
+	}
 
-		for {
-			time.Sleep(time.Second * time.Duration(rand.Intn(15)))
-			msgs <- s.generateMessage()
-		}
-	}()
-
-	return msgs, nil
+	return nil
 }
