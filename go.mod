aaa6854346b98f30614a16f1160563bb5c246b6c
Vojtech Vitek <vojtech.vitek@gmail.com>
2022-09-27 12:08:33 +0200

    Switch to github.com/rs/cors

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.19
 
 require (
 	github.com/go-chi/chi/v5 v5.0.7
-	github.com/go-chi/cors v1.2.1
+	github.com/rs/cors v1.8.2
 )
 
 require (
--- server-go/main.go
+++ server-go/main.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
+	"github.com/rs/cors"
 )
 
 func main() {
